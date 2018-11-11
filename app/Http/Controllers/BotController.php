8a644ccc05d53db941cacd41ac3828fc6885a32d
miebakso <matthew_ariel_wangsit@yahoo.com>
2018-11-11 15:39:18 +0700

    asdasd

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -5,18 +5,36 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use LINE\LINEBot;
 use LINE\LINEBot\HTTPClient\CurlHTTPClient;
-use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
 use Illuminate\Support\Facades\Log;
+use App\Line
 
 class BotController extends Controller
 {
     public function reply(Request $request){
-    	Log::debug("testtttttttttttttttttttttttttttttt");
     	Log::debug($request);
     	$replyToken = $request['events']['0']['replyToken'];
+    	Log::debug($replyToken);
         $client = new CurlHTTPClient(env('LINE_BOT_ACCESS_TOKEN'));
         $bot = new LINEBot($client, ['channelSecret' => env('LINE_BOT_SECRET')]);
-        $textMessageBuilder = new TextMessageBuilder('hello');
-        $bot->replyText($replyToken, $textMessageBuilder);
-    }
+        $text = $request['events']['0']['message']['text'];
+        Log::debug($text);
+        $arr = explode(" ",$text);
+        $result = '';
+        $command = $arr[0];
+        if($command == 'load' ){
+        	
+        } elseif($command == 'save'){
+
+        } elseif($command == 'boss'){
+
+        } elseif($command == 'NoBoss'){
+        	
+        }
+        $response = $bot->replyText($replyToken, $result);
+        if ($response->isSucceeded()) {
+		    return;
+		}
+		Log::debug($response->getHTTPStatus());
+		Log::debug($response->getRawBody());
+	}
 }
---
+++ database/seeds/BossSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class BossSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
